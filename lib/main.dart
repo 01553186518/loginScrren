d0087e07966fc80b904c6b3688ad9628ce55f4f6
mido <emadsayed117@gmail.com>
2022-03-05 14:45:48 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:untitled13/screen/login.dart';
+
+
+
+
+
+void main() {
+  runApp(const ANimathion());
+}
+
+class ANimathion extends StatelessWidget {
+  const ANimathion({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Go APP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),debugShowCheckedModeBanner: false,
+      home:MyCustomWidget(),
+    );
+  }
+}
+
+class MyCustomWidget extends StatefulWidget {
+  @override
+  _MyCustomWidgetState createState() => _MyCustomWidgetState();
+}
+
+
+
+class _MyCustomWidgetState extends State<MyCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SecondScreen();
+
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  bool _a = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 700), () {
+      setState(() {
+        _a = !_a;
+      });
+    });
+    Timer(Duration(milliseconds: 2000), () {
+      Navigator.of(context)
+          .pushReplacement(SlideTransitionAnimation(HomePage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 2000),
+            child: Lottie.asset(
+                'lib/assets/view/image/97383-yellow-delivery-guy.json',height: 2000,width: 2000),color: Colors.white,
+          ),Text('welcome to delvery')
+
+        ],
+      ),
+    );
+  }
+}
+
+class SlideTransitionAnimation extends PageRouteBuilder {
+  final Widget page;
+
+  SlideTransitionAnimation(this.page)
+      : super(
+      pageBuilder: (context, animation, anotherAnimation) => page,
+      transitionDuration: Duration(milliseconds: 2000),
+      transitionsBuilder: (context, animation, anotherAnimation, child) {
+        animation = CurvedAnimation(
+          curve: Curves.fastLinearToSlowEaseIn,
+          parent: animation,
+        );
+        return SlideTransition(
+          position: Tween(begin: Offset(1.0, 0.0), end: Offset(0.0, 0.0))
+              .animate(animation),
+          textDirection: TextDirection.rtl,
+          child: page,
+        );
+      });
+}
+
+
